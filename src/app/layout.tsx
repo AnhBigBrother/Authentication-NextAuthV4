36b12ab277b650b0ad9d8f3262f998d247d8667f
BigBruhh <anh.bigbrother@gmail.com>
2024-06-11 23:08:29 +0700

    update: background

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default async function RootLayout({
 
   return (
     <html lang='en'>
-      <body className={inter.className}>
+      <body className={`${inter.className} bg-sky-200`}>
         <NextAuthProvider session={session}>{children}</NextAuthProvider>
         <Toaster />
       </body>
